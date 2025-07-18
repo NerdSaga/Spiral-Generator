6bd61ff84e7bbf461223d0d6f14c08f5eb6afcdb
Brennan Sage <brennansage01@gmail.com>
2025-07-18 16:11:17 -0700

    No longer using vite.

---
+++ lab.js
@@ -0,0 +1,43 @@
+import { Point } from "./src/math.js"
+
+
+// Custom variables
+let radUnit = 1.6180339887498949
+let rad = 0
+// let a = 0
+let phi = 1.6180339887498949
+let b = 0.05
+let a = 0
+//
+
+
+/**
+ * 
+ * @param {*} point 
+ */
+export function start(point) {
+    point.x = 0
+    point.y = 0
+}
+
+/**
+ * 
+ * @param {Point} point 
+ * @returns 
+ */
+export function calcNext(point) {
+    point.x = (a + b * rad) * Math.cos(rad)
+    point.y = (a + b * rad) * Math.sin(rad)
+    rad += radUnit
+}
+
+
+
+
+/**
+ * Settings
+ */
+export const drawSpeed = 1
+export const pointLimit = 40000
+export const pointSize = 2
+export const lineColor = "#aaaaaa00"
---
+++ src/index.js
@@ -0,0 +1,92 @@
+import { Point } from "./math.js";
+import { calcNext, drawSpeed, lineColor, pointLimit, pointSize, start } from "../lab.js";
+const canvas = document.createElement("canvas")
+canvas.id = "gfxSurface";
+canvas.width = 2000
+canvas.height = 2000
+document.body.appendChild(canvas)
+
+const gfx = canvas.getContext("2d")
+gfx.strokeStyle = "#ffffff"
+gfx.translate(canvas.width/2, canvas.height/2)
+gfx.save()
+gfx.lineWidth = 2
+
+
+
+
+
+let pen = new Point(0, 0)
+let nextPen = new Point(0, 0)
+
+let points = [
+]
+
+
+
+function draw() {
+    gfx.fillStyle = "#ff5555"
+    gfx.strokeStyle = lineColor
+    gfx.beginPath()
+
+    for (let i = 1; i < points.length; i++) {
+        const point = points[i];
+        gfx.lineTo(point.x, point.y)
+    }
+
+    gfx.lineTo(pen.x, pen.y)
+
+    gfx.stroke()
+
+    for (let i = 0; i < points.length; i++) {
+        const point = points[i];
+        gfx.fillRect(point.x-pointSize/2, point.y-pointSize/2, pointSize, pointSize)
+    }
+
+    gfx.fillStyle = "#55ff55"
+    gfx.fillRect(pen.x-5, pen.y-5, 10, 10)
+
+}
+
+function penAtNext() {
+    const distX = nextPen.x - pen.x
+    const distY = nextPen.x - pen.x
+
+    if (distX < 1 && distX > -1) {
+        if (distY < 1 && distY > -1) {
+            return true
+        }
+    }
+
+    return false
+}
+
+let pointLimitReached = false
+function loop() {
+    gfx.fillStyle = "#123456"
+    gfx.fillRect(-canvas.width, -canvas.height, canvas.width*2, canvas.height*2)
+
+    pen.x += (nextPen.x - pen.x) * drawSpeed
+    pen.y += (nextPen.y - pen.y) * drawSpeed
+
+    if (penAtNext()) {
+        pen.x = nextPen.x
+        pen.y = nextPen.y
+        points.push(new Point(pen.x, pen.y))
+
+        if (points.length > pointLimit) {
+            console.log("Met point limit: " + pointLimit)
+            pointLimitReached = true
+        }
+
+        calcNext(nextPen)
+    }
+
+    draw()
+
+    if (!pointLimitReached) {
+        requestAnimationFrame(loop)
+    }
+}
+start(nextPen)
+loop()
